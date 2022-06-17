240d7f2b193f9cf0104da1445ef8ef6562bc5456
ShamainM <106832716+ShamainM@users.noreply.github.com>
2022-06-17 13:41:47 -0700

    Update rooms.py

--- finalproject/rooms.py
+++ finalproject/rooms.py
@@ -23,3 +23,46 @@ moanalocations= {
                        'return' : 'Heart of Tefiti',
                        }, 
                 }
+threelittlepigslocation= {
+               'Moma Pig': {
+                        'north' : 'The Woods',
+                        'east' :  'Vendor with supplies',
+                        'item' :  'straw',
+                        },
+               'Woods': {
+                        'north' : 'Another vendor with supplies', 
+                        'item' : 'sticks',
+                        },
+               'Deep Woods' : {
+                       'north' : 'Vendor with supplies',
+                       'item' : 'bricks',
+                       'item' : 'food',
+                       'item' : 'pots of boiling water',
+                       'item' : 'fire'
+                       },
+               'Little Pig 1' : {
+                       'southeast' : 'House of straw',
+                       'south' : 'Little Pig 2 House',
+                       'north' : 'Little Pig 3 House',
+                       },
+               'Little Pig 2': {
+                       'southeast' : 'House of sticks',
+                        'south' : 'Big Bad Wolf is outside',
+                       'southwest': 'Run to Little Pig 2 House',
+                       'north':  'Little Pig 3 House',
+                       },   
+               'Little Pig 3' : {
+                        'north':  'Little Pig 3 House of Bricks',
+                        'south': 'Fireplace',
+                        'item':  'Fire'
+                        'item':  'Pot of boiling water',
+                        'item':  'Food',
+                      }
+              'Big Bad Wolf: {
+                      'southeast':  'Runs to the house of straw huffs and puffs and blows the house down',
+                      'southwest':  'Runs to the house of sticks huffs and puffs and blows the house down',
+                      'north':  'Runs to the house of bricks and huffs and puffs with no success',
+                      'up' : 'Climbs on top of the roof to enter the chimney and gets burned in boiling water and shoots out to the sky'
+                      }
+  
+  
